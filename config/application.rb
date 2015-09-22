a42df89b2d6e8635b9b722c3a87702f9ad0beb80
thelrner <peteryhua@gmail.com>
2015-09-22 16:49:22 -0400

    Fixes applicationRB.

--- config/application.rb
+++ config/application.rb
@@ -25,8 +25,8 @@ module Trak
 
     config.paperclip_defaults = {
       :storage => :s3,
+      :bucket => ENV["s3_bucket"],
       :s3_credentials => {
-        :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"]
       }
